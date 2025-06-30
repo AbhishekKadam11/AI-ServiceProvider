8dbf2a74f9cfb676044564b2b5d11023ffe361f3
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2025-06-30 23:02:29 +0530

    Added gen ai module

--- chat/chat-controller.ts
+++ chat/chat-controller.ts
@@ -1,31 +1,44 @@
 import express from "express";
 import { ChatService } from "../common/shared/service/chat.service";
+import { GoogleGenAI } from "@google/genai";
 
 export class ChatController {
-    constructor(private chatService: ChatService, public readonly router = express.Router()) {
+
+    constructor(private chatService: ChatService, public readonly router = express.Router(), private genAI = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY || '' })) {
         this.chatContainer = this.chatContainer.bind(this)
-        this.router.post("/data/:sloc", this.chatContainer);
+        this.router.get("/gemini", this.chatContainer);
+        // this.model = this.genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     }
 
     private async chatContainer(req: express.Request, res: express.Response) {
         let responseObj: any = {};
-        const sloc = req.params.sloc || "";
         try {
             const validationHandler = (error: { statusCode: number, response: any }) => {
                 responseObj.statusCode = error.statusCode;
                 responseObj.response = error.response;
             }
+
             //call service
-            const result = '';//await this.chatService.saveToFile(validationHandler, sloc, req.body);
-            if (result && responseObj.statusCode == undefined) {
-                responseObj.statusCode = 204;
-            }
+            // const result = '';//await this.chatService.saveToFile(validationHandler, sloc, req.body);
+            // if (result && responseObj.statusCode == undefined) {
+            //     responseObj.statusCode = 204;
+            // }
+            // const prompt = "Create 5 funny and witty jokes about generative AI";
+            const result = await this.genAI.models.generateContent({
+                model: "gemini-2.0-flash",
+                contents: "Tell me a story in 300 words.",
+            });
+
+            console.log('result', result)
+            // const text = response.text();
+            // res.send(text);
+            responseObj.response = result;
         } catch (err) {
             responseObj.statusCode = 500;
             responseObj.response = { reason: err || "Something went wrong" };
         } finally {
-            if (responseObj.statusCode === 204) {
-                res.send(responseObj.statusCode);
+            if (responseObj.response.candidates) {
+                res.send(responseObj.response);
             } else {
                 res.status(responseObj.statusCode).send(responseObj.response);
             }
