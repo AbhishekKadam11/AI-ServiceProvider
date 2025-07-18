c384a44676f00f8430206722c29731f0b6cd8d5b
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2025-07-18 21:15:47 +0530

    Added langchain module

--- common/config/google-genai.ts
+++ common/config/google-genai.ts
@@ -1,7 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 import { AppEnvVariables } from "../shared/component/app-env-variables";
 
-
 export class GoogleGenAIConfig {
     protected genAI: GoogleGenAI;
 
