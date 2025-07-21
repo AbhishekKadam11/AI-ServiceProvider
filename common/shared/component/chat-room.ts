9e93f871aa5b0637b2722ee341be2ef6381bc5f3
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2025-07-21 23:00:41 +0530

    Fix langchain output response structure

--- common/shared/component/chat-room.ts
+++ common/shared/component/chat-room.ts
@@ -23,7 +23,7 @@ export class ChatRoom implements SocketInterface {
             // socket.emit('ping', 'Hi! I am a live socket connection');
             const io = WebSocket.getInstance();
             const testData = {
-                text: result,
+                text: result.messages[result.messages.length - 1].content,
                 date: new Date(),
                 reply: false,
                 type: 'text',
